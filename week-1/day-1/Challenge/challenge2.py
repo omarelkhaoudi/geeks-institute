6f9d2639c2e2523a261d5b7ba3a6c6ad166e0b52
omarelkhaoudi <omarelkhaoudi7@gmail.com>
2025-08-11 20:57:42 +0200

    change name files

--- week-1/day-1/Challenge/challenge2.py
+++
@@ -1,8 +0,0 @@
-user_string = input("Enter a string: ")
-result = ""
-if user_string:
-    result = user_string[0]
-for char in user_string[1:]:
-    if char != result[-1]:
-        result += char
-print("String after removing consecutive duplicates:", result)
