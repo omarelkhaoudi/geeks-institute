ac6e9c6014a242e6e3d73f8c1cec01f8d7729795
omarelkhaoudi <omarelkhaoudi7@gmail.com>
2025-08-12 19:51:02 +0200

    update exercices

--- week-1/day-2/exercices/exercices.py
+++ week-1/day-2/exercices/exercices.py
@@ -35,6 +35,20 @@
 #     }
 # }
 # brand["number_stores"] = 2
-
+# print(f"Zara's clients are {', '.join(brand['type_of_clothes'])}.")
+# brand["country_creation"] = "Spain"
+# if "international_competitors" in brand:
+#     brand["international_competitors"].append("Desigual")
+# del brand["creation_date"]
+# print("Last international competitor:", brand["international_competitors"][-1])
+# print("Major clothes colors in the US:", brand["major_color"]["US"])
+# print("Number of key-value pairs:", len(brand))
+# print("Keys of the dictionary:", list(brand.keys()))
+# more_on_zara = {
+#     "creation_date": 1975,
+#     "number_stores": 10000
+# }
+# brand.update(more_on_zara)
+# print("Number of stores after update:", brand["number_stores"])
 
 
