6fa5b7d7d9af2293240d65a3f36176af03f51a76
omarelkhaoudi <omarelkhaoudi7@gmail.com>
2025-08-13 15:13:54 +0200

    add exercice2

--- week-1/day-3/exercices/exercices.py
+++ week-1/day-3/exercices/exercices.py
@@ -15,3 +15,30 @@
 #     return oldest
 # oldest_cat = get_oldest_cat(cat1, cat2, cat3)
 # print(f"The oldest cat is {oldest_cat.name}, and is {oldest_cat.age} years old.")
+
+# exercice2
+
+class Dog:
+    def __init__(self,name,height):
+        self.name = name 
+        self.height = height
+    
+    def bark(self):
+        print(f"{self.name} goes woof!")
+
+    def jump(self):
+        print(f"{self.name} jumps {self.height*2} cm high!")
+
+davids_dog = Dog("Rex",50)
+
+print(f"name: {davids_dog.name}, height: {davids_dog.height}")
+davids_dog.bark()
+davids_dog.jump()
+
+sarahs_dog = Dog("Teacup",20)
+
+print(f"name: {sarahs_dog.name}, height: {sarahs_dog.height}")
+sarahs_dog.bark()
+sarahs_dog.jump()
+
+
