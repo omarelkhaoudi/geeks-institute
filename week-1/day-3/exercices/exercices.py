7993ef6dbd063983b0e38ef4ef0111d7d418abff
omarelkhaoudi <omarelkhaoudi7@gmail.com>
2025-08-13 15:16:04 +0200

    update exercice1

--- week-1/day-3/exercices/exercices.py
+++ week-1/day-3/exercices/exercices.py
@@ -1,20 +1,20 @@
 # # exercice1
 
-# class Cat:
-#     def __init__(self, cat_name, cat_age):
-#         self.name = cat_name
-#         self.age = cat_age
-# cat1 = Cat("Minou", 2)
-# cat2 = Cat("Mimi", 3)
-# cat3 = Cat("Tigrou", 4)
-# def get_oldest_cat(*cats):
-#     oldest = cats[0]
-#     for cat in cats:
-#         if cat.age > oldest.age:
-#             oldest = cat
-#     return oldest
-# oldest_cat = get_oldest_cat(cat1, cat2, cat3)
-# print(f"The oldest cat is {oldest_cat.name}, and is {oldest_cat.age} years old.")
+class Cat:
+    def __init__(self, cat_name, cat_age):
+        self.name = cat_name
+        self.age = cat_age
+cat1 = Cat("Minou", 2)
+cat2 = Cat("Mimi", 3)
+cat3 = Cat("Tigrou", 4)
+def get_oldest_cat(*cats):
+    oldest = cats[0]
+    for cat in cats:
+        if cat.age > oldest.age:
+            oldest = cat
+    return oldest
+oldest_cat = get_oldest_cat(cat1, cat2, cat3)
+print(f"The oldest cat is {oldest_cat.name}, and is {oldest_cat.age} years old.")
 
 # exercice2
 
