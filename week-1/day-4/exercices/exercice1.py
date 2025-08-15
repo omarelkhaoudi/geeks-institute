0feafab7b812e6016b56b90cb5e77184bc4ce7f2
omarelkhaoudi <omarelkhaoudi7@gmail.com>
2025-08-15 15:10:20 +0200

    update exercice1

--- week-1/day-4/exercices/exercice1.py
+++ week-1/day-4/exercices/exercice1.py
@@ -1,18 +1,15 @@
 class Pets():
     def __init__(self, animals):
         self.animals = animals
-
     def walk(self):
         for animal in self.animals:
             print(animal.walk())
 
 class Cat():
     is_lazy = True
-
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
     def walk(self):
         return f'{self.name} is just walking around'
     
@@ -33,9 +30,7 @@ cat2 = Chartreux("Milo", 5)
 cat3 = Siamese("Luna", 2)
 
 all_cats = [cat1, cat2, cat3]
-
 sara_pets = Pets(all_cats)
-
 sara_pets.walk()
 
 
