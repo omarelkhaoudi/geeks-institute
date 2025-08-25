e12b5638331071620987695b0a7492c91fef228f
omarelkhaoudi <omarelkhaoudi7@gmail.com>
2025-08-25 14:34:04 +0200

    update main

--- week-2/day-4/exercicesxp/exercice1/main.py
+++ week-2/day-4/exercicesxp/exercice1/main.py
@@ -1,23 +1,6 @@
 from menu_item import MenuItem
 from menu_manager import MenuManager
 
-item = MenuItem('CheeseBurger', 10)
-item.save()
-
-item.update('Veggie Burger', 20)
-
-item.delete()
-
-item2 = MenuManager.get_by_name('Beef Stew')
-print(item2.name, item2.price) if item2 else print("Item not found")
-
-items = MenuManager.all_items()
-for i in items:
-    print(i.name, i.price)
-
-from menu_item import MenuItem
-from menu_manager import MenuManager
-
 item = MenuItem('Burger', 35)
 item.save()
 
