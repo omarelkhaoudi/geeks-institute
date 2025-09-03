ba8f24f415db5a54ee7dfda4aa8152e728593d9b
omarelkhaoudi <omarelkhaoudi7@gmail.com>
2025-09-03 15:29:38 +0200

    add exercice3 exercicesxp

--- week-3/day-3/exercicesxp/exercice3.js
+++ week-3/day-3/exercicesxp/exercice3.js
@@ -11,3 +11,30 @@ function getBold_items() {
     allBoldItems = document.querySelectorAll("strong");
 }
 getBold_items();
+
+// 3) Create a function called highlight()
+// that changes the color of all the bold text to blue.
+
+function highlight() {
+    for (let i = 0; i < allBoldItems.length; i++) {
+        allBoldItems[i].style.color = "blue";
+    }
+}
+
+// 4) Create a function called returnItemsToDefault()
+// that changes the color of all the bold text back to black.
+
+function returnItemsToDefault() {
+    for (let i = 0; i < allBoldItems.length; i++) {
+        allBoldItems[i].style.color = "black";
+    }
+}
+
+// 5) Call the function highlight()
+// on mouseover (ie. when the mouse pointer is moved onto the paragraph),
+//  and the function returnItemsToDefault() on mouseout
+// (ie. when the mouse pointer is moved out of the paragraph).
+
+let p = document.querySelector("p");
+p.addEventListener("mouseover", highlight);
+p.addEventListener("mouseout", returnItemsToDefault);
