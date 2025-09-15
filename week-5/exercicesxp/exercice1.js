a97b684410bcc0208157e117ba77cf87e2ac4230
omarelkhaoudi <omarelkhaoudi7@gmail.com>
2025-09-15 18:59:27 +0200

    add exercice1 exercicesxp week-5 day-1

---
+++ week-5/exercicesxp/exercice1.js
@@ -0,0 +1,25 @@
+// Comparison
+// Instructions
+
+function compareToTen(num) {
+  return new Promise((resolve, reject) => {
+    if (num <= 10) {
+      resolve(`${num} is less than or equal to 10`);
+    } else {
+      reject(`${num} is greater than 10`);
+    }
+  });
+}
+
+
+// Test cases
+
+compareToTen(15)
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
+// Output: "15 is greater than 10"
+
+compareToTen(8)
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
+// Output: "8 is less than or equal to 10"
