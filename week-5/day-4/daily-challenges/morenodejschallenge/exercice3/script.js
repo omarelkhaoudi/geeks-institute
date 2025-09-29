d5d3f0199b876a7cb13bd9f6b69a201a963d9512
omarelkhaoudi <omarelkhaoudi7@gmail.com>
2025-09-29 09:53:01 +0200

    update more node.js challenges

--- week-5/day-4/daily-challenges/morenodejschallenge/exercice3/script.js
+++ week-5/day-4/daily-challenges/morenodejschallenge/exercice3/script.js
@@ -3,8 +3,8 @@ const nextHoliday = require("./date");
 
 const result = nextHoliday();
 
-console.log(`📅 Today is: ${result.today}`);
+console.log(`Today is: ${result.today}`);
 console.log(
   `🎉 The next holiday is ${result.holiday} (${result.holidayDate})`
 );
-console.log(`⏳ Time left: ${result.remaining}`);
+console.log(`Time left: ${result.remaining}`);
